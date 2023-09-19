d6ead05903d3dd79199a24942fe081e0355f7df8
nicholass003 <veganicholas123@gmail.com>
2023-09-19 22:38:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## NetheriteAntiKnockback
+
+A simple plugin for netherite armor to disable knockback.
+
+## Credits
+
+<a target="_blank" href="https://icons8.com/icon/zTdQhmS0nXJZ/armor">Armor</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ src/slayerretrun/netheriteantiknockback/Main.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ *   _____ _                       _____      _                    
+ *  / ____| |                     |  __ \    | |                   
+ * | (___ | | __ _ _   _  ___ _ __| |__) |___| |_ _ __ _   _ _ __  
+ *  \___ \| |/ _` | | | |/ _ \ '__|  _  // _ \ __| '__| | | | '_ \ 
+ *  ____) | | (_| | |_| |  __/ |  | | \ \  __/ |_| |  | |_| | | | |
+ * |_____/|_|\__,_|\__, |\___|_|  |_|  \_\___|\__|_|   \__,_|_| |_|
+ *                  __/ |                                          
+ *                 |___/                                           
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * @author SlayerRetrun Team
+ * @link https://github.com/Slayer-Return
+ * 
+ * 
+ */
+
+declare(strict_types=1);
+
+namespace slayerretrun\netheriteantiknockback;
+
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+    protected function onEnable() : void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onKnockBack(EntityDamageByEntityEvent $event) : void
+    {
+        $victim = $event->getEntity();
+        if ($this->isPlayerUseNetheriteArmor($victim)){
+            $victim->setKnockBack(0.0);
+            $victim->setVerticalKnockBackLimit(0.0);
+        }
+    }
+
+    public function isPlayerUseNetheriteArmor(Player $player) : bool
+    {
+        $playerArmorInventory = $player->getArmorInventory();
+        if ($playerArmorInventory->getHelmet()->getTypeId() === ItemTypeIds::NETHERITE_HELMET &&
+            $playerArmorInventory->getHelmet()->getTypeId() === ItemTypeIds::NETHERITE_CHESTPLATE &&
+            $playerArmorInventory->getHelmet()->getTypeId() === ItemTypeIds::NETHERITE_LEGGINGS &&
+            $playerArmorInventory->getHelmet()->getTypeId() === ItemTypeIds::NETHERITE_BOOTS){
+            return true;
+        }
+        return false;
+    }
+}
